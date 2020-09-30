e62f30a5ee4b2e38448e6ed7c8d387ddeb909da9
Igor <igor.mm09@hotmail.com>
2020-09-30 14:30:17 -0300

    Add complex phasor capability function

--- NPMtoLBOMM.py
+++ NPMtoLBOMM.py
@@ -1,8 +1,12 @@
-def lbomcompat(A,decimal_separator=','):
+import cmath
+import numpy as np
+
+def lbomcompat(A,compmode='normal',decimal_separator=','):
     B = []
     for i in range(A.shape[0]):
         for j in range(A.shape[1]):
-            B.append(str(A[i,j]))
+            if compmode == 'normal': B.append(str(A[i,j]))
+            elif compmode == 'ang': B.append(comptophas(A[i,j]))
             B.append(" # ")
         del B[-1]
         B.append(" ## ")
@@ -18,3 +22,10 @@ def lbomcompat(A,decimal_separator=','):
         else:
             L.append(i)
     return ("left [ matrix { "+"".join(L) + " } right ]")
+
+
+def comptophas(Value,angtyp='deg',decimal=4):
+    absvalue = abs(Value)
+    if angtyp == 'deg': ang = cmath.phase(Value)*(180/cmath.pi)
+    elif angtyp == 'rad': ang = cmath.phase(Value)
+    return("{0:.{decimal}f}⦟{1:.{decimal}f}".format(absvalue,ang,decimal=decimal))
